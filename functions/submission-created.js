e90af559ed08820bf0b7e730a63ddc22fc999cf9
Andrew <ahernandez@codeforamerica.org>
2021-01-05 18:53:55 -0800

    updated structure

---
+++ functions/submission-created.js
@@ -0,0 +1,61 @@
+const worker = Tesseract.create();
+
+const defaultImage = 'https://tesseract.projectnaptha.com/img/eng_bw.png';
+// const defaultImage = 'https://i.redd.it/8h66m4nnyo331.jpg';
+// const defaultImage = 'https://i.imgur.com/fun6Hrl.png';
+
+
+const ocr = document.querySelector('#ocr');
+const input = ocr.querySelector('#ocr__input');
+const img = ocr.querySelector('#ocr__img');
+const output = ocr.querySelector('#ocr__output');
+const form = ocr.querySelector('#ocr__form');
+
+
+input.value = defaultImage;
+img.src = defaultImage;
+
+
+function myError(err, message) {	
+	console.warn(`MyError: ${message || ''}`);
+	console.error(err);
+}
+
+async function recognizeImage(image) {
+	console.log('recognize START');
+	worker.terminate();
+	
+	output.textContent = 'Waiting to start.';
+	output.classList.remove('error');
+	output.classList.add('processing');
+	
+	return worker.recognize(image)
+		.progress(progress => {
+			// console.log('progress', progress);
+			output.innerHTML = `Processing...<br>Status: ${progress.status}<br>${Math.round(progress.progress * 100)}%`;
+		}).then(result => {
+			// console.log('result', result);
+
+			output.classList.remove('processing');
+			output.textContent = result.text;
+		}).catch(err => {
+			myError(err, 'caught error');
+			output.classList.add('error');
+			output.textContent = 'Error processing image.';
+		}).finally(() => {
+			console.log('recognize END');		
+			worker.terminate();
+		});
+}
+
+form.onsubmit = async e => {
+	console.log('submit START');
+	e.preventDefault();
+	
+	const imageUrl = input.value;
+	img.src = imageUrl;
+	
+	await recognizeImage(imageUrl);
+	
+	console.log('submit END');
+}
